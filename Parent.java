cf32cc3d86ccb4d7edf1de849c4e83927ca9d078
pallabojha <ojhapallab@gmail.com>
2017-04-12 16:41:04 +0530

    Assingment4.1
    Assingment4.1

---
+++ Parent.java
@@ -0,0 +1,15 @@
+
+ class Parent {
+    private void method1(){
+ System.out.println ("Parent's method1()");
+}
+public void method2() {
+ System.out.println ("Parent's method2()");
+method1();
+}
+}
+ class Child extends Parent {
+ public void method1() {
+ System.out.println ("Child's method1()");
+} 
+}
